e32f115cc7194144e034c80a9c0b2977519d0231
zachglick <glickzachary@gmail.com>
2019-02-25 18:05:40 -0500

    check Psi4 version

--- snsmp2/snsmp2.py
+++ snsmp2/snsmp2.py
@@ -56,7 +56,7 @@ def run_sns_mp2(name, molecule, **kwargs):
     if len(kwargs) > 0:
         raise ValueError('Unrecognized options: %s' % str(kwargs))
 
-    if parse_version(psi4.__version__) <= parse_version('1.3a1') :
+    if parse_version(psi4.__version__) < parse_version('1.3rc2') :
         raise ValueError('Psi4 {:s} is not compatible (v1.3+ necessary)'.format(psi4.__version__))
 
     # Force to c1
